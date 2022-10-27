c5d260d7bfb716038dbbbff7ddf56c9d27d3c3a2
Robert Oliveira <86692878+Robert-Oliveira@users.noreply.github.com>
2022-10-26 21:34:52 -0300

    adicionado validators

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,8 @@ import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculoValidators';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,24 +11,44 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamentos!: Pensamento[];
+  formulario!: FormGroup;
 
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formbuilder: FormBuilder
+  ) {}
 
-  constructor(private service: PensamentoService, private router: Router) {}
-
-  ngOnInit(): void {}
-
-  criarPensamento() {
-    this.service.criar(this.pensamento).subscribe(() => {
-      this.router.navigate(['/listarPensamento']);
+  ngOnInit(): void {
+    this.formulario = this.formbuilder.group({
+      autoria: [
+        '',
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(3),
+          minusculoValidator,
+        ]),
+      ],
     });
   }
+
+  criarPensamento() {
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
+        this.router.navigate(['/listarPensamento']);
+      });
+    }
+  }
+
   cancelar() {
     this.router.navigate(['/listarPensamento']);
+  }
+
+  habilitarBotao(): string {
+    if (this.formulario.valid) {
+      return 'botao';
+    } else {
+      return 'botao__desabilitado';
+    }
   }
 }
